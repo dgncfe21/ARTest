9c6a5ca77650110d3e47c75b0ff71735626c926a
dgncfe21 <85706641+dgncfe21@users.noreply.github.com>
2022-01-12 09:07:01 -0600

    Create placesvok.js

---
+++ placesvok.js
@@ -0,0 +1,50 @@
+
+window.onload = () => {
+    let method = 'static';
+
+    // if you want to statically add places, de-comment following line:
+    // method = 'static';
+    if (method === 'static') {
+        let places = staticLoadPlaces();
+        return renderPlaces(places);
+    }
+
+    if (method !== 'static') {
+        // first get current user location
+        return navigator.geolocation.getCurrentPosition(function (position) {
+
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: "CFE",
+            location: {
+                lat: 25.697931, // change here latitude if using static data
+                lng: -100.318311, // change here longitude if using static data, 
+            }
+        },
+    ];
+}
+
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        // add place name 
+        let text = document.createElement('a-link');
+        text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        text.setAttribute('title', place.name);
+        text.setAttribute('href', 'http://www.example.com/');
+        text.setAttribute('scale', '15 15 15');
+
+        text.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded', { detail: { component: this.el }}))
+        });
+
+        scene.appendChild(text);
+    });
+};
